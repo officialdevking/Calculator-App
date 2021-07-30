f99bd921d0fda38807d8529279d080036af6446f
officialdevking <kingsleysunday998@gmail.com>
2021-07-30 11:26:24 -0700

    first commit

---
+++ javascript/apps.js
@@ -0,0 +1,18 @@
+const keys = document.querySelector('.calculator_keys')
+const display = document.querySelector('.calculator_display')
+keys.addEventListener('click', e =>{
+    if(!e.target.closest('button')) 
+
+    return
+
+    const key = e.target;
+    const keyValue = key.textContent;
+    const displayValue = display.textContent;
+    
+
+    if(displayValue == '0'){
+        display.textContent = keyValue
+    }else{
+        display.textContent = displayValue
+    }
+})
